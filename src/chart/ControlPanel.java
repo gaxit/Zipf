e9eb5ccafae28a474f7bcbe4a0c14951e1351a86
gaxit <gaxit@interia.eu>
2015-04-29 13:02:51 +0200

    Dodanie aktualizowania informacji o wynikach

--- src/chart/ControlPanel.java
+++ src/chart/ControlPanel.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,7 +50,7 @@ public class ControlPanel extends JPanel {
 		prepareAndAddLoadDataButton(frame);
 		prepareAndAddMinDataSpinner();
 		add(LabelBox);
-		setResultLabelValues(0, 0, 0, 0);
+		setResultLabelValues(0, 0, 0);
 	}
 
 	private Box prepareAndAddResultLabels() {
@@ -63,12 +66,20 @@ public class ControlPanel extends JPanel {
 		return box;
 	}
 
-	private void setResultLabelValues(int words, double average, double stdDev,
-			double coef) {
+	private void setResultLabelValues(int words, double average, double stdDev) {
+		double coef = stdDev / average;
+		if (average == 0) {
+			coef = 0;
+		}
+		MathContext mathContext = new MathContext(4, RoundingMode.HALF_UP);
+		BigDecimal averageBig = new BigDecimal(average, mathContext);
+		BigDecimal stdDevBig = new BigDecimal(stdDev, mathContext);
+		BigDecimal coefBig = new BigDecimal(coef, mathContext);
 		wordsLabel.setText("Iloœæ s³ów: " + words);
-		stdDevLabel.setText("Œrednia: " + average);
-		averageLabel.setText("Odchylenie standardowe: " + stdDev);
-		coefficientLabel.setText("Odchylenie standardowe / œrednia : " + coef);
+		stdDevLabel.setText("Œrednia: " + averageBig.toString());
+		averageLabel.setText("Odchylenie standardowe: " + stdDevBig.toString());
+		coefficientLabel.setText("Odchylenie standardowe / œrednia : "
+				+ coefBig.toString());
 	}
 
 	private void prepareAndAddMinDataSpinner() {
@@ -104,10 +115,16 @@ public class ControlPanel extends JPanel {
 							.getAbsolutePath());
 
 					TextAnalyzer textAnalyzer = new TextAnalyzer();
-					List<Word> limitedWords = new ArrayList<Word>();
-					limitedWords.addAll(textAnalyzer
+					List<Word> limitedWords = textAnalyzer
 							.removeWordWithInstancesLessThanThreshold(
-									extractedWords, threshold));
+									extractedWords, threshold);
+
+					double average = textAnalyzer.averageZipfValue(
+							limitedWords, threshold);
+					double stdDev = textAnalyzer
+							.stdDev(limitedWords, threshold);
+
+					setResultLabelValues(limitedWords.size(), average, stdDev);
 
 					if (frame.getContentPane().getComponents().length == 2) {
 						frame.getContentPane().remove(1);
